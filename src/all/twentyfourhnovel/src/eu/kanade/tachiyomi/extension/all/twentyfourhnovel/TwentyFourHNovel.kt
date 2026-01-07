3075ad4a4701a838a6dcab2e52bcdc59ef64e7b4
IhsanHaiderCL16 <either.haider.ihsan@gmail.com>
2026-01-07 14:26:41 +0600

    Update TwentyFourHNovel.kt

--- src/all/twentyfourhnovel/src/eu/kanade/tachiyomi/extension/all/twentyfourhnovel/TwentyFourHNovel.kt
+++ src/all/twentyfourhnovel/src/eu/kanade/tachiyomi/extension/all/twentyfourhnovel/TwentyFourHNovel.kt
@@ -1,4 +1,4 @@
-package eu.kanade.tachiyomi.extension.all.twentyfourhnovel
+package eu.kanade.tachiyomi.extension.en.twentyfourhnovel
 
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.FilterList
