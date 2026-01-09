496e9a3d77f54b7d9338e2360f39fd389e8c1fb4
IhsanHaiderCL16 <either.haider.ihsan@gmail.com>
2026-01-09 12:54:00 +0600

    Update build.gradle.kts

--- src/all/twentyfourhnovel/build.gradle.kts
+++ src/all/twentyfourhnovel/build.gradle.kts
@@ -8,9 +8,6 @@ ext {
     set("extName", "24hNovel")
     set("extClass", ".TwentyFourHNovel")
     set("extVersionCode", 1)
-    set("extVersionName", "1.0.0")
-    set("pkgNameSuffix", "en.twentyfourhnovel")
-    set("extLib", "")
 }
 
 apply(from = "$rootDir/common.gradle")
